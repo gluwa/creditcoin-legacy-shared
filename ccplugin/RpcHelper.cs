88edd04edf6adec707425d27c39f999bf8876e41
Will <83000144+will-gluwa@users.noreply.github.com>
2021-11-01 14:06:50 -0700

    Update RpcHelper.cs

--- ccplugin/RpcHelper.cs
+++ ccplugin/RpcHelper.cs
@@ -39,16 +39,16 @@ namespace ccplugin
         private const string HEADER = "header";
         private const string BLOCK_NUM = "block_num";
 
-        public const string creditCoinNamespace = "8a1a04";
-        public const string settingNamespace = "000000";
-        public const string walletPrefix = "0000";
-        public const string addressPrefix = "1000";
-        public const string transferPrefix = "2000";
-        public const string askOrderPrefix = "3000";
-        public const string bidOrderPrefix = "4000";
-        public const string dealOrderPrefix = "5000";
-        public const string repaymentOrderPrefix = "6000";
-        public const string offerPrefix = "7000";
+        public static string creditCoinNamespace = "8a1a04";
+        public static string settingNamespace = "000000";
+        public static string walletPrefix = "0000";
+        public static string addressPrefix = "1000";
+        public static string transferPrefix = "2000";
+        public static string askOrderPrefix = "3000";
+        public static string bidOrderPrefix = "4000";
+        public static string dealOrderPrefix = "5000";
+        public static string repaymentOrderPrefix = "6000";
+        public static string offerPrefix = "7000";
 
         public static string CompleteBatch(HttpClient httpClient, string host, string path, ByteArrayContent content, bool txid, out string continuation)
         {
